a7deb3b1cfccbb67e3ab4bf2832b813482ab48b4
TymorIbrahim <tymoribrahim@campus.technion.ac.il>
2022-04-26 19:48:58 +0300

    inputting players

--- src/com/company/SnakesAndLaddersGame.java
+++ src/com/company/SnakesAndLaddersGame.java
@@ -5,6 +5,7 @@ public class SnakesAndLaddersGame {
     Player [] players = new Player[5];
     int player_count = 0;
     static String add_player = "add player ";
+    int max_num_of_players = 5;
 
     public SnakesAndLaddersGame (int min, int max){
         this.die = new Die(min, max);
@@ -22,7 +23,7 @@ public class SnakesAndLaddersGame {
         return true;
     }
 
-    String getPlayerNameFromInput (String input){
+    public String getPlayerNameFromInput (String input){
         String player_name = "";
         int i = add_player.length();
         while (input.charAt(i) != ' ') {
@@ -58,19 +59,53 @@ public class SnakesAndLaddersGame {
         return color;
     }
 
-    public void initializeGame (){
-        String input = Main.scanner.nextLine();
-        while (!input.equals("end")){
-            //System.out.println("player added 1");
-            if (contains(input, add_player)){
-                System.out.println("player added");
-                String new_player_name = getPlayerNameFromInput(input);
-                Color new_player_color = getColorFromInput(input, new_player_name);
-                Player new_player = new Player(new_player_name, new_player_color);
-                players [player_count] = new_player;
-                player_count++;
+    public boolean isLegalPlayer (Player player){
+        if (player_count >= max_num_of_players){
+            System.out.println("The maximal number of players is five !");
+            return false;
+        }
+        for (int i = 0; i < player_count; i++) {
+            if (player.getName().equals(players[i].getName())) {
+                if (player.getGamePiece().getColor().equals(players[i].getGamePiece().getColor())) {
+                    System.out.println("The name and the color are already taken!");
+                    return false;
+                } else {
+                    System.out.println("The name is already taken!");
+                    return false;
+                }
             }
+            else if(player.getGamePiece().getColor().equals(players[i].getGamePiece().getColor())){
+                System.out.println("The color is already taken!");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void addNewPlayer (String input){
+        String new_player_name = getPlayerNameFromInput(input);
+        Color new_player_color = getColorFromInput(input, new_player_name);
+        Player new_player = new Player(new_player_name, new_player_color);
+        if (isLegalPlayer(new_player)) {
+            System.out.println("player added");
+            players[player_count] = new_player;
+            player_count++;
+        }
+    }
+
+    public void initializeGame (){
+        String input = "";
+        while (!input.equals("end")){
             input = Main.scanner.nextLine();
+            if (contains(input, add_player)){
+                addNewPlayer(input);
+            }
+            if(input.equals("end")){
+                if (player_count < 2){
+                    System.out.println("Cannot start the game, there are less then two players!");
+                    input = "";
+                }
+            }
         }
         if (input.equals("end")) {
             System.out.println("end of input");
@@ -80,4 +115,6 @@ public class SnakesAndLaddersGame {
             }
         }
     }
+
+
 }
